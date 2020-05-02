c9148e739a2442ec69ae6bbb4ca66416b823faa3
Anshu Kumar Shandilya <ashandilya64@gmail.com>
2020-05-02 22:45:11 +0530

    Task (2) Completed

--- Lesson02-GitHub-Repo-Search/T02.04-Exercise-ConnectingToTheInternet/app/src/main/java/com/example/android/datafrominternet/MainActivity.java
+++ Lesson02-GitHub-Repo-Search/T02.04-Exercise-ConnectingToTheInternet/app/src/main/java/com/example/android/datafrominternet/MainActivity.java
@@ -56,7 +56,7 @@ public class MainActivity extends AppCompatActivity {
         String githubQuery = mSearchBoxEditText.getText().toString();
         URL githubSearchUrl = NetworkUtils.buildUrl(githubQuery);
         mUrlDisplayTextView.setText(githubSearchUrl.toString());
-        // TODO (2) Call getResponseFromHttpUrl and display the results in mSearchResultsTextView
+        // COMPLETED (2) Call getResponseFromHttpUrl and display the results in mSearchResultsTextView
         String githubSearchResults = null;
         try{
             githubSearchResults = NetworkUtils.getResponseFromHttpUrl(githubSearchUrl);
@@ -64,7 +64,7 @@ public class MainActivity extends AppCompatActivity {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        // TODO (3) Surround the call to getResponseFromHttpUrl with a try / catch block to catch an IOException
+        // COMPLETED (3) Surround the call to getResponseFromHttpUrl with a try / catch block to catch an IOException
     }
 
     @Override
